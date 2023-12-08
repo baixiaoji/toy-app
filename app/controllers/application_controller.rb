350acc15d7e1138118b2ef16f2f5057176c7a02f
ajiea.chou <1041874421@qq.com>
2023-12-08 09:54:38 +0800

    feat: mvc 中 c流程,req -> c -> m -> v

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+    def hello
+        render html: "¡hola, ajiea!"
+    end
+
+
+    def goodbye
+        render html: "bye wolrd"
+    end
 end
